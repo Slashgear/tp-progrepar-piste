c4975c956d08ae3139f22248bd63f671eb35d41c
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-06-10 19:24:01 +0200

    AGO: logger added at ErrorPage

--- WebApp/src/main/java/com/polytech4a/piste/controller/components/ErrorPage.java
+++ WebApp/src/main/java/com/polytech4a/piste/controller/components/ErrorPage.java
@@ -1,5 +1,7 @@
 package com.polytech4a.piste.controller.components;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.ui.ModelMap;
 
 /**
@@ -7,6 +9,7 @@ import org.springframework.ui.ModelMap;
  *         08/06/2015
  */
 public class ErrorPage {
+    private static final Logger logger = LogManager.getLogger(ErrorPage.class.getSimpleName());
     private static final String DIR_VIEW = "common";
     private static final String ERROR_VIEW = "error";
 
@@ -17,6 +20,7 @@ public class ErrorPage {
     public static String newError(final ModelMap pModel, String errorMessage, String errorReturnPage) {
         pModel.addAttribute("errorMessage", errorMessage);
         pModel.addAttribute("errorReturnPage", errorReturnPage);
+        logger.error(String.format("An error occured : %s", errorMessage));
         return String.format("%s/%s", DIR_VIEW, ERROR_VIEW);
     }
 }
