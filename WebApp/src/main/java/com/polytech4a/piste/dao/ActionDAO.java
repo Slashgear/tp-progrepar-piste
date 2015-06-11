4263044679e9dd292001d39fdc19a9fef91ff99a
Slashgear <antoine395.caron@laposte.net>
2015-06-11 16:44:53 +0200

    ACN:Add hasActionValidatedByApprenant request.

--- WebApp/src/main/java/com/polytech4a/piste/dao/ActionDAO.java
+++ WebApp/src/main/java/com/polytech4a/piste/dao/ActionDAO.java
@@ -2,6 +2,8 @@ package com.polytech4a.piste.dao;
 
 import com.polytech4a.piste.beans.Action;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by Antoine CARON on 04/06/2015.
@@ -10,4 +12,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @version 1.0
  */
 public interface ActionDAO extends JpaRepository<Action, Integer> {
+
+    @Query("select o.valeurdebut-a.scoremin from Obtient o inner join Action a where o.numaction=a.numaction and a.numaction= :idAction and o.numapprenant= :idApprenant")
+    Integer hasActionValidatedByApprenant(@Param("idAction") Integer idAction, @Param("idApprenant") Integer idApprenant);
 }
